eaac4efc99a8d80fdc83c0fbabbbd8dba1bb1b64
Furkan <furkansahin3180@gmail.com>
2026-03-24 14:17:20 +0300

    bug fix

--- script.js
+++ script.js
@@ -11,33 +11,47 @@ let previousInput = '';
 let operation = undefined;
 
 const updateDisplay = () => {
-    textElement.innerText = currentInput;
+    if (currentInput === "") {
+        textElement.innerText = "0";
+    } else {
+        textElement.innerText = currentInput;
+    }
 }
 
 
 numberButtons.forEach(button => {
-    
+    button.addEventListener("click", () => {
 
-    button.addEventListener("click", () => { 
-        if (button.innerText === "," && currentInput.includes(",")) {
+        if (button.innerText === ",") {
+            let sayilar = currentInput.split(/[+\-x÷%]/);
+            let sonSayi = sayilar[sayilar.length - 1];
+
+            if (sonSayi.includes(",")) return;
+        }
+
+
+        if (currentInput === "" && button.innerText === ",") {
+            currentInput = "0,";
+            updateDisplay();
             return;
         }
-        if (button.innerText === operatorButtons && currentInput.includes(operatorButtons)) {
-            return;
+
+        if (currentInput === "0" && button.innerText !== ",") {
+            currentInput = button.innerText;
+        } else {
+            currentInput += button.innerText;
         }
-        currentInput += button.innerText;
+
         updateDisplay();
-        
-    }); 
-
-}); 
+    });
+});
 
 operatorButtons.forEach(button => {
 
     button.addEventListener("click", () => {
         if (currentInput === "") return;
         let lastchar = currentInput.slice(-1);
-        if(['+', '-', 'x', '÷', '%'].includes(lastchar)) {
+        if (['+', '-', 'x', '÷', '%'].includes(lastchar)) {
             currentInput = currentInput.slice(0, -1) + button.innerText;
             updateDisplay();
             return;
@@ -46,20 +60,20 @@ operatorButtons.forEach(button => {
         updateDisplay();
     });
 
-    
+
 
 });
 
-    clearButton.addEventListener("click", () => {
-        currentInput = "";
-        updateDisplay();
-    });
+clearButton.addEventListener("click", () => {
+    currentInput = "";
+    updateDisplay();
+});
 
 
-    deleteButton.addEventListener("click", () => {
-        currentInput = currentInput.slice(0, -1);
-        updateDisplay();
-    });
+deleteButton.addEventListener("click", () => {
+    currentInput = currentInput.slice(0, -1);
+    updateDisplay();
+});
 
 
 
@@ -69,11 +83,16 @@ themeSwitch.addEventListener("change", () => {
 
 equalButton.addEventListener("click", () => {
     try {
-        let islem = currentInput.replace(/x/g, '*').replace(/÷/g, '/');
-        
-        currentInput = eval(islem).toString();
+        let islem = currentInput
+            .replace(/x/g, '*')
+            .replace(/÷/g, '/')
+            .replace(/,/g, '.');
+
+        let sonuc = eval(islem).toString().replace(/\./g, ',');
+
+        currentInput = sonuc;
         updateDisplay();
-        
+
     } catch (error) {
         currentInput = "Hata";
         updateDisplay();
